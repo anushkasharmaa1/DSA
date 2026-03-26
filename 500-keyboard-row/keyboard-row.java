df7eda0f5879c9a68fcc3d23833527cf66f66e12
Anushka Sharma <anushkasharma1785@gmail.com>
2026-03-26 14:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (27.95%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String[] findWords(String[] words) {
+
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        List<String> result = new ArrayList<>();
+
+        for(String word : words){
+
+            String w = word.toLowerCase();
+
+            if(check(w, row1) || check(w, row2) || check(w, row3))
+                result.add(word);
+        }
+
+        return result.toArray(new String[0]);
+    }
+
+    private boolean check(String word, String row){
+        for(char c : word.toCharArray()){
+            if(row.indexOf(c) == -1)
+                return false;
+        }
+        return true;
+    }
+}
